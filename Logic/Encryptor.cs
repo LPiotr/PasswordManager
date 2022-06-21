91134938af3d32bb6e78d6d463f173caae150c1d
LPiotr1 <piotr.lebelt1@gmail.com>
2022-06-22 00:29:06 +0200

    pierwsze zmiany, stworzenie działającego algorytmu hashującego. Do zrobienia: -formularze -reszta logiki odpowiedzialnej za modyfikacje hasel oraz magazynowanie uzytkownikow.

---
+++ Logic/Encryptor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Konscious.Security.Cryptography;
+using System.IO;
+
+namespace PasswordManager.Logic
+{
+    internal class EncryptorAndDecryptor
+    {
+        private byte[] CreateSalt()
+        {
+            var buffer = new byte[16];
+            var rng = new RNGCryptoServiceProvider();
+            var fileInfo = new FileInfo("salt.txt");
+
+            if (File.Exists("salt.txt")  && fileInfo.Length > 5)
+            {
+                rng.GetBytes(buffer);
+                File.ReadAllBytes("salt.txt");
+                return buffer;
+            }
+            else
+            {
+                rng.GetBytes(buffer);
+                File.WriteAllBytes("salt.txt", buffer);
+                return buffer;
+            }
+        }
+
+        // implementacja metody kasującej stary plik z solą
+
+        private byte[] HashPassword(string password, byte[] salt)
+        {
+            
+            var argon2id = new Argon2id(Encoding.UTF8.GetBytes(password));
+
+
+            argon2id.Salt = salt;
+            
+
+            argon2id.DegreeOfParallelism = 4;
+            argon2id.Iterations = 4;
+            argon2id.MemorySize = 1024 * 1024; //1GB
+
+            return argon2id.GetBytes(16);
+        }
+    private bool VerifyHash(string password, byte[] salt, byte[] hash)
+        {
+            var newHash = HashPassword(password, salt);
+            return hash.SequenceEqual(newHash);
+        }
+    public byte[] Enrypt(string password)
+        {
+            var salt = CreateSalt();
+            var hash = HashPassword(password, salt);
+            
+            var success = VerifyHash(password, salt, hash);
+            File.WriteAllBytes("hash.txt", hash);
+            return hash;
+            
+        }
+
+    //public byte[] Decrypt(string password)
+    //    {
+            
+    //    }
+    }
+}
---
+++ login.cs
@@ -0,0 +1,27 @@
+﻿using PasswordManager.Logic;
+using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordManager
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var encryptobj = new EncryptorAndDecryptor();
+            encryptobj.Enrypt("inputalamakota");
+        }
+    }
+}
