a184f647dae2dbfff28c4dec33e9b87129f148b7
LPiotr1 <piotr.lebelt1@gmail.com>
2022-06-25 20:58:44 +0200

    poprawki w kodzie i background color

--- Register.Designer.cs
+++ Register.Designer.cs
@@ -102,7 +102,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(155, 211);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(236, 211);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
--- login.Designer.cs
+++ login.Designer.cs
@@ -94,6 +94,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(203, 158);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.textBox2);
